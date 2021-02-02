fa919e05e19875b8f8a9c7f136f64ea92b65c2d6
Mewan <mewan.chathuranga@snappymob.com>
2021-02-02 17:37:12 +0800

    Initial Commit

---
+++ Africa Udemy/Africa Udemy/Africa_UdemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_UdemyApp.swift
+//  Africa Udemy
+//
+//  Created by Mewan on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Africa_UdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
